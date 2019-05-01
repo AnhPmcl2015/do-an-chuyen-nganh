7c13d54a3a7369c30cb0c401275b8894c91e86b0
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2019-05-01 23:25:58 +0700

    xong giao diện mobile

--- parent-module/client-module/src/main/java/com/uit/mobile/mobile_module/component/QrScan.js
+++ parent-module/client-module/src/main/java/com/uit/mobile/mobile_module/component/QrScan.js
@@ -3,27 +3,30 @@ import React, {Component} from 'react';
 import {
     Text,
     View,
-    Linking,
+    Image,
     TouchableHighlight,
     PermissionsAndroid,
     Platform,
     StyleSheet,
     AsyncStorage,
-    Alert
+    Alert,
+    TouchableOpacity
 } from 'react-native';
 
 import {CameraKitCameraScreen} from 'react-native-camera-kit';
-import { Header } from 'react-native-elements';
+import {Header, Overlay, Divider} from 'react-native-elements';
+import HeaderCustom from './Header';
 
 export default class QRScan extends Component {
     constructor() {
         super();
         this.state = {
-            //variable to hold the qr value
             qrvalue: '',
             hostIp: '',
             soDienThoai: '',
-            opneScanner: false
+            opneScanner: false,
+            diaDiem: '',
+            openOverlay: true
         };
     }
 
@@ -51,17 +54,16 @@ export default class QRScan extends Component {
         } catch (error) {}
     }
 
-    onOpenlink() {
-
-        this
-            .props
-            .navigation
-            .navigate('LoginForm');
-    }
     onBarcodeScan(qrvalue) {
         //called after te successful scanning of QRCode/Barcode
         this.setState({qrvalue: qrvalue});
         this.setState({opneScanner: false});
+    }
+
+    closeOverlay = () => {
+        this.setState({
+            openOverlay: false
+        });
     }
 
     onOpneScanner() {
@@ -81,7 +83,6 @@ export default class QRScan extends Component {
                     }
                 } catch (err) {
                     alert("Camera permission err", err);
-                    console.warn(err);
                 }
             }
             requestCameraPermission();
@@ -91,40 +92,173 @@ export default class QRScan extends Component {
         let displayModal;
         if (!this.state.opneScanner) {
             return (
-
                 <View>
-                    <Header />
+                    <HeaderCustom/>
+
+                    <View style={styles.container}>
+                        <Text
+                            style={{
+                            color: '#696969',
+                            marginLeft: 15,
+                            marginTop: 20,
+                            marginBottom: 20,
+                            fontSize: 16
+                        }}>
+                            Công cụ
+                        </Text>
+
+                        <TouchableHighlight onPress={() => this.onOpneScanner}>
+
+                            <View style={styles.qrButtonContainer}>
+
+                                <Image
+                                    style={{
+                                    width: 50,
+                                    height: 50,
+                                    marginRight: 5
+                                }}
+                                    source={{
+                                    uri: 'https://png.icons8.com/qrcode'
+                                }}/>
+
+                                <View
+                                    style={{
+                                    flexWrap: 'wrap',
+                                    flex: 1
+                                }}>
+                                    <Text
+                                        style={{
+                                        fontSize: 16,
+                                        color: 'black'
+                                    }}>Quét mã QR</Text>
+                                    <Text
+                                        style={{
+                                        fontSize: 12,
+                                        color: '#696969'
+                                    }}>Theo dõi hành vi gửi yêu cầu đánh giá là từ chính bản thân của bạn</Text>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+
+                    <Overlay height={400} isVisible={this.state.openOverlay}>
+
+                        <View>
+                            <View
+                                style={{
+                                alignItems: 'center'
+                            }}>
+                                <Text
+                                    style={{
+                                    width: 200,
+                                    fontSize: 18,
+                                    marginBottom: 30,
+                                    textAlign: 'center',
+                                    marginTop: 30
+                                }}>Thông tin yêu cầu gửi bản đánh giá</Text>
+                            </View>
+
+                            <View
+                                style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginBottom: 10
+                            }}>
+                                <Text
+                                    style={{
+                                    fontSize: 17,
+                                    color: 'black'
+                                }}>Host IP:
+                                </Text>
+                                <Text
+                                    style={{
+                                    fontSize: 16,
+                                    color: '#696969',
+                                    marginLeft: 15
+                                }}>192.168.1.1</Text>
+                            </View>
+
+                            <Divider
+                                style={{
+                                backgroundColor: '#696969'
+                            }}/>
+
+                            <View
+                                style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginBottom: 10,
+                                marginTop: 10
+                            }}>
+                                <Text
+                                    style={{
+                                    fontSize: 17,
+                                    color: 'black'
+                                }}>Số điện thoại:
+                                </Text>
+                                <Text
+                                    style={{
+                                    fontSize: 16,
+                                    color: '#696969',
+                                    marginLeft: 15
+                                }}>0783550324</Text>
+                            </View>
+
+                            <Divider
+                                style={{
+                                backgroundColor: '#696969'
+                            }}/>
+
+                            <View
+                                style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginBottom: 10,
+                                marginTop: 10
+                            }}>
+                                <Text
+                                    style={{
+                                    fontSize: 17,
+                                    color: 'black'
+                                }}>Địa điểm:
+                                </Text>
+                                <Text
+                                    style={{
+                                    fontSize: 16,
+                                    color: '#696969',
+                                    marginLeft: 15,
+                                    width: 200
+                                }}>55/15T, khu phố 7, phường tân thới nhất, quận 12, thành phố hồ chí minh</Text>
+                            </View>
+
+                            <Divider
+                                style={{
+                                backgroundColor: '#696969'
+                            }}/>
+
+                            <View
+                                style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginBottom: 10,
+                                marginTop: 30,
+                                justifyContent: 'space-around'
+                            }}>
+                                <TouchableOpacity style={styles.btnQuayLai} onPress={()=>this.closeOverlay()}>
+                                    <Text style={{fontSize: 15}}>Quay lại</Text>
+                                </TouchableOpacity>
+
+                                <TouchableOpacity style={styles.btnDongY}>
+                                    <Text style={{fontSize: 15, color: 'white'}}>Đồng ý</Text>
+                                </TouchableOpacity>
+                            </View>
+
+                        </View>
+                    </Overlay>
                 </View>
 
-//                 <View style={styles.container}>
-//                     <Text style={styles.heading}>React Native QR Code Example</Text>
-//                     <Text style={styles.simpleText}>{this.state.qrvalue
-//                             ? 'Scanned QR Code: ' + this.state.qrvalue
-//                             : ''}</Text>
-//                     {this
-//                         .state
-//                         .qrvalue
-//                         .includes("http")
-//                         ? <TouchableHighlight onPress={() => this.onOpenlink()} style={styles.button}>
-//                                 <Text
-//                                     style={{
-//                                     color: '#FFFFFF',
-//                                     fontSize: 12
-//                                 }}>Open Link</Text>
-//                             </TouchableHighlight>
-//                         : null
-// }
-//                     <TouchableHighlight onPress={() => this.onOpneScanner()} style={styles.button}>
-//                         <Text
-//                             style={{
-//                             color: '#FFFFFF',
-//                             fontSize: 12
-//                         }}>
-//                             Open QR Scanner
-//                         </Text>
-//                     </TouchableHighlight>
-//                 </View>
             );
+
         }
         return (
             <View style={{
@@ -135,7 +269,13 @@ export default class QRScan extends Component {
                     laserColor={'blue'} //Color can be of your choice
                     frameColor={'yellow'} //If frame is visible then frame color
                     colorForScannerFrame={'black'} //Scanner Frame color
-                    onReadCode={event => this.onBarcodeScan(event.nativeEvent.codeStringValue)}/>
+                    onReadQRCode={event => this.onBarcodeScan(event.nativeEvent.codeStringValue)} cameraOptions={{
+                    flashMode: 'auto',
+                    focusMode: 'on',
+                    zoomMode: 'on',
+                    ratioOverlay: '1:1',
+                    ratioOverlayColor: '#00000077'
+                }}/>
             </View>
         );
     }
@@ -143,30 +283,33 @@ export default class QRScan extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        backgroundColor: '#DCDCDC',
+        height: '100%'
+    },
+
+    qrButtonContainer: {
+        backgroundColor: 'white',
+        flexDirection: 'row',
         alignItems: 'center',
+        paddingLeft: 15,
+        height: 100
+    },
+
+    btnQuayLai: {
+        borderWidth: 1,
+        borderRadius: 30,
+        borderColor: '#696969',
+        width: 100,
+        height: 30,
         justifyContent: 'center',
-        backgroundColor: 'white'
+        alignItems: 'center'
     },
-    button: {
-        alignItems: 'center',
-        backgroundColor: '#2c3539',
-        padding: 10,
-        width: 300,
-        marginTop: 16
-    },
-    heading: {
-        color: 'black',
-        fontSize: 24,
-        alignSelf: 'center',
-        padding: 10,
-        marginTop: 30
-    },
-    simpleText: {
-        color: 'black',
-        fontSize: 20,
-        alignSelf: 'center',
-        padding: 10,
-        marginTop: 16
+    btnDongY: {
+        borderRadius: 30,
+        backgroundColor: '#00B5EC',
+        width: 100,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
--- parent-module/client-module/src/main/java/com/uit/mobile/mobile_module/index.js
+++ parent-module/client-module/src/main/java/com/uit/mobile/mobile_module/index.js
@@ -3,8 +3,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import AvatarCustom from './component/Avatar';
-import Logout from './component/Logout';
-import HeaderCustom from './component/Header';
 
-AppRegistry.registerComponent(appName, () => HeaderCustom);
+AppRegistry.registerComponent(appName, () => App);
