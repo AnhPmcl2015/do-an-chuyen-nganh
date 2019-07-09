7643c8f8ff8c9ae6252d45ee0ada9a7ef612a4f8
christianvu191 <50440846+christianvu191@users.noreply.github.com>
2019-07-09 13:01:27 +0700

    login
    update login

--- parent-module/client-module/src/main/java/com/uit/mobile/mobile_module/App.js
+++ parent-module/client-module/src/main/java/com/uit/mobile/mobile_module/App.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import LoginForm from './component/LoginForm';
 import QRScan from './component/QrScan';
-
+import Login from './component/Login';
 
 
 const Navigation = createStackNavigator({
     LoginForm: LoginForm,
-    QRScan: QRScan
-}, {initialRouteName: 'QRScan'});
+    QRScan: QRScan,
+    Login: Login
+}, {initialRouteName: 'Login'});
 
 const App = createAppContainer(Navigation);
 
